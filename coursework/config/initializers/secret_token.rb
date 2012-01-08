2b4000bff0935f429dcbf37460676c54b5a15ef5
damtur <damtur@interia.pl>
2012-01-08 17:56:08 +0000

    firts commit

---
+++ coursework/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coursework::Application.config.secret_token = '03d00affeef573f9e5f03d261782810e63421523fad4c50ced449e44cbe84362ba186d501a2421aa73f1a1299c73d1947bbcbf8bfb23cbc563f2d8ad3283294e'
